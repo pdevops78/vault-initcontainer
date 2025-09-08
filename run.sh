c0edab3e866bd6cada13ca91732b6bc51bdcdaf3
Your Name <vemasani.pavani@gmail.com>
2025-09-08 14:57:57 +0530

    helm chart

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ vault login $VAULT_TOKEN &>/dev/null
  vault kv get $SECRET_NAME |sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^Key|^--'| awk '{print "export "$1"="$2}' >/data/secrets
 
 
-# vault kv get common/common | sed -n -e '/= Data =/,$p' | grep -Ev '= Data =|^key|^---'| awk '{print "export "$1"="$2}'
+
